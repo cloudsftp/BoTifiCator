ba59eb7f7c5649a13ca6ebe09f0947469efe274e
Fabian Weik <fabian.weik@protonmail.com>
2026-03-10 22:14:47 +0100

    add weekly moving averages table

--- sql/30-crate-weekly-avg.sql
+++ sql/30-crate-weekly-avg.sql
@@ -15,3 +15,16 @@ ALTER MATERIALIZED VIEW
 SET
     (timescaledb.materialized_only = false)
 ;
+
+CREATE OR REPLACE VIEW
+    btc_moving_avg_1week
+AS SELECT
+    time_bucket('1 week', week) as week,
+    avg(average) OVER(ORDER BY week ROWS BETWEEN 199 PRECEDING AND CURRENT ROW) as moving_average_200,
+    avg(average) OVER(ORDER BY week ROWS BETWEEN  99 PRECEDING AND CURRENT ROW) as moving_average_100
+FROM
+    btc_avg_1week
+ORDER BY
+    week
+DESC
+;
